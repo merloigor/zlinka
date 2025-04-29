150be65b64bc91d9108249528082e36f59c67195
merloigor <merloigor@gmail.com>
2025-04-28 23:38:51 -0300

    ajuste de rota

--- build/src/http/server.js
+++ build/src/http/server.js
@@ -6,10 +6,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const fastify_1 = __importDefault(require("fastify"));
 const fastify_type_provider_zod_1 = require("fastify-type-provider-zod");
 const env_1 = require("../env");
+const create_url_1 = require("./routes/create-url");
 const port = env_1.env.PORT;
 const app = (0, fastify_1.default)().withTypeProvider();
 app.setValidatorCompiler(fastify_type_provider_zod_1.validatorCompiler);
 app.setSerializerCompiler(fastify_type_provider_zod_1.serializerCompiler);
+app.register(create_url_1.createUrlRoute);
 app.listen({
     port: port,
     host: '0.0.0.0'
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,7 @@
 import fastify from "fastify";
 import { serializerCompiler, validatorCompiler, ZodTypeProvider } from "fastify-type-provider-zod";
 import { env } from "../env";
+import { createUrlRoute } from "./routes/create-url";
 
 const port = env.PORT;
 
@@ -8,6 +9,8 @@ const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
+
+app.register(createUrlRoute);
 
 app.listen({
     port: port as number,
